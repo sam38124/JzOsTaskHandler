3789563fb7a81f10466d3fe73039b2b0b4969351
sam38124 <sam38124@gmail.com>
2020-08-03 17:03:40 +0800

    0803

--- JzOsTaskHandler/TaskHandler.swift
+++ JzOsTaskHandler/TaskHandler.swift
@@ -7,7 +7,30 @@
 //
 
 import Foundation
+@available(iOS 10.0, *)
 open class TaskHandler:callback{
+    public func closeTimer(_ tag: String) {
+        let needCancel=timer.filter({ $0.tag==tag })
+        if(needCancel.count>0){
+            for i in needCancel{
+                i.timer!.invalidate()
+            }
+            timer=timer.filter({$0.tag != tag})
+        }
+        
+    }
+    public func runTaskTimer(_ tag: String, _ delay: Double, _ runner:@escaping () -> Void) {
+        if(timer.filter({ $0.tag==tag }).count==0){
+            let timerClass=TimerClass()
+            timerClass.tag=tag
+            timerClass.timer=Timer.scheduledTimer(withTimeInterval: delay, repeats: true, block: {_ in
+                runner()
+            })
+            timer.append(timerClass)
+        }
+    }
+    
+    var timer=[TimerClass]()
     static var instance:callback? = nil
     var clockinstance=JzClock()
     var runtag=[String]()
@@ -16,6 +39,7 @@ open class TaskHandler:callback{
         if(instance==nil){
             instance=TaskHandler()
         }
+        
         return instance!
     }
     open func clock() -> JzClock {
@@ -28,15 +52,17 @@ open class TaskHandler:callback{
         }else{
             runtag.append(tag)
             runner()
+            var temptag=[String]()
             for i in 0..<runtag.count{
-                if(runtag[i]==tag){
-                    runtag.remove(at: i)
+                if(runtag[i] != tag){
+                    temptag.append(runtag[i])
                 }
             }
+            runtag=temptag
         }
     }
     
-    open func runTaskTimer(_ tag: String, _ time: Double, _ runner: () -> Void) {
+    open func runTaskDelay(_ tag: String, _ time: Double, _ runner: () -> Void) {
         for i in runnerTimer{
             if(i.tag==tag){
                 if(i.timer.stop()<time){
@@ -79,4 +105,8 @@ open class TaskHandler:callback{
 open class TimerTask{
     var timer:JzClock=JzClock()
     var tag:String = ""
+}
+open class TimerClass{
+    var timer:Timer? = nil
+    var tag = ""
 }
--- JzOsTaskHandler/callback.swift
+++ JzOsTaskHandler/callback.swift
@@ -13,7 +13,11 @@ public protocol callback{
        //執行任務處理，並且同時間只能處理一次任務，必須等任務處理完才能進行下次的執行
        func runTaskOnce(_ tag:String,_ runner:()->Void)
        //執行任務處理，並且必須大於時間間隔才可以再次執行
-       func runTaskTimer(_ tag:String,_ time:Double,_ runner:()->Void)
+       func runTaskDelay(_ tag:String,_ time:Double,_ runner:()->Void)
        //執行任務處理，並且限制可同時執行數量
        func runTaskMultiple(_ tag:String,_ count:Int,_ runner:()->Void)
+       //執行迴圈任務處理，如沒有此tag則開始新任務，反之則不進行新任務處理
+       func runTaskTimer(_ tag:String,_ delay:Double,_ runner:@escaping()->Void)
+       //關閉timer的任務處理
+       func closeTimer(_ tag:String)
 }
